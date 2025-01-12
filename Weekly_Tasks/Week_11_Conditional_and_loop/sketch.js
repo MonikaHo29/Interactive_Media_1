969e801a42aed5c3f7af10e98f64118cf650ba7b
MonikaHo29 <hocamtumonika29@gmail.com>
2025-01-12 17:32:19 +0700

    Update Weekly Task 11

--- Weekly_Tasks/Week_11_Conditional_and_loop/sketch.js
+++ Weekly_Tasks/Week_11_Conditional_and_loop/sketch.js
@@ -18,6 +18,11 @@ let isChangingText = false; // Flag to control text change
 function setup() {
   createCanvas(1500, 800);
   background(0);
+  textFont('Protest Revolution');
+  textSize(48);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  text("Hallo Welt", width / 2, height / 2);
 }
 
 // DRAW FUNCTION WITH ANIMATION
